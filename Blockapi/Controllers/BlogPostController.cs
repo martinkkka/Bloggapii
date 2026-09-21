3e80b5726d34bb097a15863d8d85ea33a785ecc9
tothm <tothm1@kkszki.hu>
2026-09-21 11:21:46 +0200

    haladunk

--- Blockapi/Controllers/BlogPostController.cs
+++ Blockapi/Controllers/BlogPostController.cs
@@ -46,5 +46,74 @@ namespace Blockapi.Controllers
 
             return blogposts;
         }
+        [HttpGet("{id}")]
+        public object GetBlogPost(int id)
+        {
+            blogpost blogpost = null;
+
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            string sql = "SELECT * FROM `blogpost` WHERE id = @id;";
+
+            var cmd = new MySqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            var data = cmd.ExecuteReader();
+
+            if (data.Read())
+            {
+                blogpost = new blogpost
+                {
+                    Id = data.GetInt32("id"),
+                    Title = data.GetString("title"),
+                    Content = data.GetString("content"),
+                    postTime = data.GetDateTime("postTime"),
+                    updateTime = data.GetDateTime("updateTime"),
+                    blogId = data.GetInt32("blogId")
+                };
+            }
+
+            connection.Close();
+
+            if (blogpost == null)
+            {
+                return NotFound("Nincs ilyen blogpost.");
+            }
+
+            return blogpost;
+        }
+    
+    [HttpPost]
+        public object CreateBlogPost(blogpost blogpost)
+        {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            string sql = @"
+                INSERT INTO blogpost
+                (title, content, postTime, updateTime, blogId)
+                VALUES
+                (@title, @content, @postTime, @updateTime, @blogId);
+            ";
+
+            var cmd = new MySqlCommand(sql, connection);
+
+            cmd.Parameters.AddWithValue("@title", blogpost.Title);
+            cmd.Parameters.AddWithValue("@content", blogpost.Content);
+            cmd.Parameters.AddWithValue("@postTime", DateTime.Now);
+            cmd.Parameters.AddWithValue("@updateTime", DateTime.Now);
+            cmd.Parameters.AddWithValue("@blogId", blogpost.blogId);
+
+            cmd.ExecuteNonQuery();
+
+            blogpost.Id = (int)cmd.LastInsertedId;
+            blogpost.postTime = DateTime.Now;
+            blogpost.updateTime = DateTime.Now;
+
+            connection.Close();
+
+            return Ok(blogpost);
+        }
     }
 }
